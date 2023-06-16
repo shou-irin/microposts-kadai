2a9923a5d913b1adcda291472d59fbd374ace4c5
shou.irin <yiling-zhang@dac.co.jp>
2023-06-16 01:47:57 +0000

    fix favorites view

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -7,31 +7,12 @@
             @include('users.card')
         </aside>
         <div class="sm:col-span-2 mt-4">
-            {{-- タブ --}}  
+            {{-- タブ --}}
             @include('users.navtabs')
-            {{-- お気に入り一覧 --}}
             <div class="mt-4">
-                @if (isset($favorites))
-                    <ul class="list-unstyled">
-                        @foreach ($favorites as $favorite)
-                            <li class="flex items-start gap-x-2 mb-4">
-                                <div class="avatar">
-                                    <div class="w-12 rounded">
-                                        <img src="{{ Gravatar::get($favorite->user->email) }}" alt="" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div>
-                                        <a class="link link-hover text-info" href="{{ route('users.show', $favorite->user->id) }}">{{ $favorite->user->name }}</a>
-                                        <span class="text-muted text-gray-500">posted at {{ $favorite->created_at }}</span>
-                                    </div>
-                                    <div>
-                                        <p class="mb-0">{!! nl2br(e($favorite->content)) !!}</p>
-                                    </div>
-                                </div>
-                            </li>
-                        @endforeach
-                    </ul>
+                <h2>Favorites</h2>
+                @if (count($favorites) > 0)
+                    @include('microposts.microposts', ['microposts' => $favorites])
                 @endif
             </div>
         </div>
